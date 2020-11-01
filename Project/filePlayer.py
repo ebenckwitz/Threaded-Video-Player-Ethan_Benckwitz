25ea3be8913fefc16fda73a3f17ec8447c6547b8
Ethan <ebenckwitz>
2020-10-31 21:08:09 -0600

    added thread class

--- Project/filePlayer.py
+++ Project/filePlayer.py
@@ -5,7 +5,27 @@ import threading
 import cv2, os, base64, queue
 import numpy as np
 
+class QueueThread:
+    def __init__(self):
+        self.queue = []
+        self.full = threading.Semaphore(24)
+        self.empty = threading.Semaphore(0)
+        self.lock = threading.Lock()
 
+    def enqueue(self, item):
+        self.empty.acquire()
+        self.lock.acquire()
+        self.queue.append(item)
+        self.lock.release()
+        self.full.release()
+
+    def dequeue(self):
+        self.full.acquire()
+        self.lock.acquire()
+        frame = queue.pop(0)
+        self.lock.release()
+        self.empty.release()
+    
 def Exracting(filename, Cframes):
     count =  0 #Initialize frame count
 
